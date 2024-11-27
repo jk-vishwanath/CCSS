ea5af479b0c2e3855ea392662a2c146fd7103ccc
jk-vishwanath <66640951+jk-vishwanath@users.noreply.github.com>
2024-11-26 23:33:44 -0500

    Update apin1.py

--- apin1.py
+++ apin1.py
@@ -18,6 +18,7 @@ nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4')  # Add this line to download omw-1.4
+nltk.download('punkt_tab')
 
 # Text preprocessing function
 def preprocess_text(text):
